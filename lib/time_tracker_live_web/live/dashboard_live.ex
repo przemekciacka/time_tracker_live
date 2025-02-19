fcb8dc73d5ede2f6853ab5f93057ca4d7358eda7
Przemek Ciąćka <przemyslaw.ciacka@gmail.com>
2025-02-19 08:45:51 +0100

    add toggle functionality to timer button

--- lib/time_tracker_live_web/live/dashboard_live.ex
+++ lib/time_tracker_live_web/live/dashboard_live.ex
@@ -1,3 +1,12 @@
 defmodule TimeTrackerLiveWeb.DashboardLive do
   use TimeTrackerLiveWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    enabled = false
+    {:ok, assign(socket, enabled: enabled)}
+  end
+
+  def handle_event("toggle", _params, socket) do
+    {:noreply, assign(socket, enabled: !socket.assigns.enabled)}
+  end
 end
