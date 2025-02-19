e513074fdd3a29580ba0c79c73122883f68b1d79
Przemek Ciąćka <przemyslaw.ciacka@gmail.com>
2025-02-19 18:12:00 +0100

    implement start/stop functionality

--- lib/time_tracker_live_web/live/dashboard_live.ex
+++ lib/time_tracker_live_web/live/dashboard_live.ex
@@ -3,10 +3,26 @@ defmodule TimeTrackerLiveWeb.DashboardLive do
 
   def mount(_params, _session, socket) do
     enabled = false
-    {:ok, assign(socket, enabled: enabled)}
+    seconds = 0
+
+    {:ok, assign(socket, enabled: enabled, seconds: seconds)}
   end
 
   def handle_event("toggle", _params, socket) do
-    {:noreply, assign(socket, enabled: !socket.assigns.enabled)}
+    toggle_timer(socket.assigns.enabled, socket)
+  end
+
+  def handle_info(:tick, socket) do
+    {:noreply, update(socket, :seconds, &(&1 + 1))}
+  end
+
+  defp toggle_timer(false, socket) do
+    {:ok, tref} = :timer.send_interval(1000, self(), :tick)
+    {:noreply, assign(socket, enabled: true, tref: tref)}
+  end
+
+  defp toggle_timer(true, socket) do
+    :timer.cancel(socket.assigns.tref)
+    {:noreply, assign(socket, enabled: false, tref: nil)}
   end
 end
