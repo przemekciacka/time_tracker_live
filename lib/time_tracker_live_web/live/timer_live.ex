29a6afccd9ee3eb1aabcee8d4f8f809d21f2db56
Przemek Ciąćka <przemyslaw.ciacka@gmail.com>
2025-04-09 17:28:57 +0200

    subscribe to timer topic and handle task start/stop events

--- lib/time_tracker_live_web/live/timer_live.ex
+++ lib/time_tracker_live_web/live/timer_live.ex
@@ -1,7 +1,11 @@
 defmodule TimeTrackerLiveWeb.TimerLive do
   use TimeTrackerLiveWeb, :live_view
 
+  @topic "timer"
+
   def mount(_params, _session, socket) do
+    TimeTrackerLiveWeb.Endpoint.subscribe(@topic)
+
     tasks = TimeTrackerLive.Task.all_tasks()
     current_task = TimeTrackerLive.Task.current_task()
     start_time = current_task && current_task.start_time || nil
@@ -15,12 +19,14 @@ defmodule TimeTrackerLiveWeb.TimerLive do
   def handle_event("toggle_timer", %{"name" => name}, %{assigns: %{start_time: nil}} = socket) do
     task = TimeTrackerLive.Task.start_task(name)
     tasks = TimeTrackerLive.Task.all_tasks()
+    TimeTrackerLiveWeb.Endpoint.broadcast_from(self(), @topic, "task_started", task);
     {:noreply, socket |> assign(:start_time, task.start_time) |> assign(:tasks, tasks)}
   end
 
   def handle_event("toggle_timer", _params, socket) do
     TimeTrackerLive.Task.stop_task()
     all_tasks = TimeTrackerLive.Task.all_tasks()
+    TimeTrackerLiveWeb.Endpoint.broadcast_from(self(), @topic, "task_stopped", %{})
     {:noreply, socket |> assign(:start_time, nil) |> assign(:tasks, all_tasks)}
   end
 
@@ -30,6 +36,24 @@ defmodule TimeTrackerLiveWeb.TimerLive do
   end
 
   def handle_event("update_name", _, socket) do
+    {:noreply, socket}
+  end
+
+  def handle_info(%{topic: @topic, event: "task_started", payload: payload}, socket) do
+    tasks = TimeTrackerLive.Task.all_tasks()
+    start_time = payload.start_time
+
+    {:noreply, socket
+     |> assign(:tasks, tasks)
+     |> assign(:start_time, start_time)}
+  end
+
+  def handle_info(%{topic: @topic, event: "task_stopped"}, socket) do
+    tasks = TimeTrackerLive.Task.all_tasks()
+    {:noreply, socket |> assign(:tasks, tasks) |> assign(:start_time, nil)}
+  end
+
+  def handle_info(_msg, socket) do
     {:noreply, socket}
   end
 end
